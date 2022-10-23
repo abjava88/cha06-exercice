d9643ff681ebeb7361d3f3134d2660f010506975
Abbes <abbes.boulebtina@gmail.com>
2022-10-23 11:44:05 -0400

    Backend react express app.

---
+++ backend/Server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+//const cors = require("cors");
+const routes = require("./routes/ToDoRoute");  
+
+const app = express();
+const PORT = process.env.PORT | 5000;
+
+app.use(express.json());
+//app.use(cors());
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+// Routes
+app.use(routes);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
